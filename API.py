fe03a10f92e98b75a9f610408862fe88b12c51f1
GustiMuhammadKhotami <gustimuhammadkhotami@gmail.com>
2024-04-15 17:09:46 +0800

    commit semua file

---
+++ API.py
@@ -0,0 +1,177 @@
+from flask import Flask, request, redirect, jsonify, Response
+import random, requests, re, json
+
+app = Flask("Ephoto360-Random-Image-Generator-For-Github")
+
+session = False
+payload = False
+
+def createSession():
+    global session, payload
+    url = "https://en.ephoto360.com/create-glossy-silver-3d-text-effect-online-802.html"
+    session = requests.Session()
+    req = session.get(url)
+    data = {
+        "autocomplete0": "",
+        "text": ["Gusti"],
+        "submit": "GO",
+        "token": re.search("name=\"token\" value=\"(.*?)\"", req.text).group(1),
+        "build_server": re.search("name=\"build_server\" value=\"(.*?)\"", req.text).group(1),
+        "build_server_id": re.search("name=\"build_server_id\" value=\"(.*?)\"", req.text).group(1),
+    }
+    res = session.post(url, data=data)
+    payload = json.loads(re.search("name=\"form_value_input\" value=\"(.*?)\"", res.text).group(1).replace("&quot;", "\""))
+    del payload["text"]
+
+class EphotoModel:
+    def __init__(self, text, session, payload):
+        self.text = text
+        self.host = "https://en.ephoto360.com"
+        self.urls = [
+            "809",
+            "797",
+            "767",
+            "589",
+            "811",
+            "808",
+            "769",
+            "768",
+            "717",
+            "706",
+            "682",
+            "673",
+            "668",
+            "658",
+            "655",
+            "619",
+            "595",
+            "594",
+            "576",
+            "521",
+            "469",
+            "441",
+            "424",
+            "153",
+            "108",
+            "85",
+            "74",
+            "59",
+            "17",
+            "807",
+            "798",
+            "704",
+            "698",
+            "695",
+            "686",
+            "685",
+            "683",
+            "618",
+            "597",
+            "582",
+            "580",
+            "577",
+            "506",
+            "397",
+            "376",
+            "372",
+            "369",
+            "359",
+            "357",
+            "356",
+            "347",
+            "343",
+            "342",
+            "341",
+            "340",
+            "329",
+            "288",
+            "285",
+            "278",
+            "248",
+            "215",
+            "210",
+            "206",
+            "204",
+            "200",
+            "199",
+            "194",
+            "188",
+            "187",
+            "184",
+            "156",
+            "147",
+            "143",
+            "139",
+            "126",
+            "111",
+            "110",
+            "109",
+            "97",
+            "88",
+            "86",
+            "84",
+            "75",
+            "73",
+            "72",
+            "69",
+            "68",
+            "63",
+            "61",
+            "30",
+        ]
+        self.session = session
+        self.payload = payload
+
+    def randomUrl(self):
+        return random.choice(self.urls)
+
+    def getImage(self):
+        if not self.payload:
+            return False
+        self.payload["text[]"] = [self.text]
+        self.payload["id"] = self.randomUrl()
+        resp = self.session.post("https://en.ephoto360.com/effect/create-image", data=payload).json()
+        return self.getRaw(self.payload["build_server"] + resp.get("image"))
+
+    def getRaw(self, urlImage):
+        rawImage = self.session.get(urlImage, stream=True).raw
+        return rawImage
+
+class EphotoView:
+    def renderview(self, response):
+        rawResponse = response.getImage()
+        if not rawResponse:
+            return False
+        return Response(rawResponse, mimetype="image/jpg")
+
+class EphotoController:
+    def __init__(self, ephotomodel, ephotoview):
+       self.ephotomodel = ephotomodel
+       self.ephotoview = ephotoview
+
+    def response(self):
+        return self.ephotoview.renderview(self.ephotomodel)
+
+@app.route("/gusti", methods=["GET"])
+def routehandler():
+    teks = request.args.get("text")
+    if not teks:
+        return jsonify({"Error": "harus menyertakan parameter text"})
+    model = EphotoModel(teks, session, payload)
+    view = EphotoView()
+    controller = EphotoController(model, view)
+    retview = controller.response()
+    return retview if retview else jsonify({"error": "server sibuk"})
+
+@app.route("/cronjob", methods=["GET"])
+def cronjobTask():
+    createSession()
+    return jsonify({"error": "cron job"})
+
+@app.route("/<apapun>", methods=["GET"])
+def redirectKeGithub(apapun):
+    return jsonify({"error": f"path /{apapun} tidak ada"})
+
+@app.route("/", methods=["GET"])
+def index():
+    return redirect("https://github.com/GustiMuhammadKhotami/GitCoolName")
---
+++ README.md
@@ -0,0 +1,29 @@
+<div align="center">
+  <h1>PyEphotoAPI</h1>
+  <span>Source Code: <a href="https://github.com/GustiMuhammadKhotami/GitCoolName">PyEphotoAPI</a></span>
+</div>
+<hr>
+<p align="center">Dapatkan efek nama keren yang dihasilkan secara dinamis untuk <strong>README.md</strong> github anda</p>
+<br>
+<h1 align="center">Testing</h1>
+<hr>
+<p>Gambar dibawah digenerate secara random dari website <strong>ephoto360</strong>. Gambar akan selalu berubah setiap kali halaman direfresh.</p>
+<p align="center">
+  <img src="https://gt-gitcoolname-api.vercel.app/gusti?text=Khotami" width="70%"/ alt="tidak bisa menampilkan image">
+</p>
+<br>
+<h1 align="center">Catatan Penting</h1>
+<hr>
+<p>Terkadang gambar tidak bisa ditampilkan ketika digunakan untuk README.md karena waktu tunggu response yang terlalu lama.</p>
+<br>
+<h1 align="center">Penggunaan</h1>
+<p>Salin dan tempel kode markdown dibawah ke kode markdown milik anda.</p>
+<p>Ubah NAMA menjadi nama anda. Gunakan + sebagai spasi jika nama anda mengandung spasi</p>
+
+    ![alt msg](https://gt-gitcoolname-api.vercel.app/gusti?text=NAMA)
+
+Atau menggunakan kode html seperti dibawah
+
+    <div align="center">
+      <img src="https://gt-gitcoolname-api.vercel.app/gusti?text=NAMA" width="70%" alt="alt msg"/>
+    </div>
